e00e6a14c6e0372101772b3a31756d559b2563b3
Babin Bohora <bohorababin1@gmail.com>
2022-02-19 23:04:09 +0900

    main tables migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_134605) do
+ActiveRecord::Schema.define(version: 2022_02_19_135011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cells", force: :cascade do |t|
+    t.bigint "column_id", null: false
+    t.bigint "row_id", null: false
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["column_id"], name: "index_cells_on_column_id"
+    t.index ["row_id"], name: "index_cells_on_row_id"
+  end
+
+  create_table "columns", force: :cascade do |t|
+    t.bigint "table_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["table_id"], name: "index_columns_on_table_id"
+  end
+
+  create_table "rows", force: :cascade do |t|
+    t.bigint "table_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["table_id"], name: "index_rows_on_table_id"
+  end
+
+  create_table "tables", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tables_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +59,9 @@ ActiveRecord::Schema.define(version: 2022_02_19_134605) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cells", "columns"
+  add_foreign_key "cells", "rows"
+  add_foreign_key "columns", "tables"
+  add_foreign_key "rows", "tables"
+  add_foreign_key "tables", "users"
 end
